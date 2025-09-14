c2d50ab81fae793e025ec4a21574e4c014bc3097
nmjoselyne09 <nmjoselyne09@gmail.com>
2025-09-15 02:13:25 +0530

    some changes to task8

--- 8-square.js
+++ 8-square.js
@@ -1,10 +1,10 @@
-// Print a square of size N using the character 'x'
-const n = parseInt(process.argv[2], 10);
+// Script to print a square of size N using 'X'
+const size = parseInt(process.argv[2], 10);
 
-if (isNaN(n)) {
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < n; i++) {
-    console.log('x'.repeat(n));
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
   }
 }
